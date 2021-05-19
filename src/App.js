88883bbb9050e0bd1ba547995af2d2edeceab6e1
Atiti1 <atitidestiny17@gmail.com>
2021-05-19 06:30:31 +0100

    editing

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Register from './components/Register/Register';
 import Rank from './components/Rank/Rank';
 import Signin from './components/Signin/Signin';
-// ATITI DESTINY
 
 
 
